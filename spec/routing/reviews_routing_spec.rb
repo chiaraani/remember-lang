8601b05a275da9f6f015b9232ae794c4b0728110
Chiara Escurra <chiaraani@gmail.com>
2018-09-12 17:17:46 +0100

    routing test

---
+++ spec/routing/reviews_routing_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe ReviewsController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(:get => "/").to route_to("reviews#index")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/words/1/reviews").to route_to("reviews#create", :word_id => '1')
+    end
+  end
+end
