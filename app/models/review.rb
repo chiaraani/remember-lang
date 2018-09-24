ca4751d569d95c00bcc23498bcb7f39d374188a1
Chiara Escurra <chiaraani@gmail.com>
2018-09-24 16:28:44 +0100

    meantime errors

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,10 @@
 class Review < ApplicationRecord
   validates :scheduled_for, presence: true
   belongs_to :word
-  scope :pending, -> { where("scheduled_for <= ?", Date.today).notdone }
-  scope :notdone, -> { where(made_at: nil) }
+  scope :pending, -> { where("scheduled_for <= ?", Date.today).notmade }
+  scope :notmade, -> { where(made_at: nil) }
 
   def meantime
-    (scheduled_for - created_at.to_date).to_i
+    ((made_at or scheduled_for).to_date - created_at.to_date).to_i
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,32 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  let(:review) {
-    Review.create!(scheduled_for: Date.new(2018),
-                   word: Word.create!(spelling: 'hello'))
-  }
+  let(:word) { Word.create!(spelling: 'hello') }
+  let(:review) { Review.create!(scheduled_for: 10.days.from_now, word: word) }
+  let(:pending_review) { Review.create!(created_at: 1.week.ago, scheduled_for: 1.day.ago, word: word) }
+  let(:made_review) { Review.create!(created_at: 1.week.ago, scheduled_for: 3.days.ago, word: word, made_at: Date.today, passed: true) }
 
   it 'belongs to a word' do
     expect(review.word).to be_a Word
   end
 
   it 'requires scheduled_for' do
-    expect(Review.create(word: Word.create!(spelling: 'hello')).errors.messages.count).to eql(1)
+    expect(Review.create(word: word).errors.messages.count).to eql(1)
   end
 
   it 'has a pending group' do
-    pending_review = review
-    Word.first.reviews.create!(scheduled_for: Date.tomorrow)
-    Word.first.reviews.create!(scheduled_for: Date.new(2017), made_at: Date.today, passed: true)
+    ids = [pending_review.id]
+    review
+    made_review
     expect(Review.pending.count).to eql(1)
-    expect(Review.pending.ids).to match [pending_review.id]
+    expect(Review.pending.ids).to match ids
   end
 
   describe 'meantime' do
-    it do
-      review = Review.create!(scheduled_for: Date.today + 10,
-                   word: Word.create!(spelling: 'hello'))
-      expect(review.meantime).to eql(10)
+    context 'the review has not been made' do
+      it { expect(review.meantime).to eql(10) }
+    end
+    context 'the review has already been made' do
+      it { expect(made_review.meantime).to eql(7) }
     end
   end
+=begin
+  describe 'previous'
+    context 'There is a previous review' do
+      before do
+        made_review
+        review
+=end
 end
