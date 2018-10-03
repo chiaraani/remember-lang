eb0dd884918aa5e228c2ed7e14bd741e85dcb99e
Chiara Escurra <chiaraani@gmail.com>
2018-10-03 14:22:21 +0100

    words order and display next review

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,7 +4,7 @@ class WordsController < ApplicationController
   # GET /words
   # GET /words.json
   def index
-    @words = Word.all
+    @words = Word.all.order(created_at: :desc)
   end
 
   # GET /words/1
