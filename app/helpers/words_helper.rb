7b3b70f8114b2849bd587a1b2f86aab161c00dad
Chiara Escurra <chiaraani@gmail.com>
2018-10-05 16:52:52 +0100

    errors when showing when a word should be reviewed

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,2 +1,11 @@
 module WordsHelper
+  def next_review(word)
+    if word.reviews.pending.count == 1
+      'It should be reviewed today'
+    elsif word.reviews.notperformed.count == 1
+      'It will be reviewed in ' + pluralize((word.reviews.last.scheduled_for - Date.today).to_i, 'day')
+    else
+      'It will NOT be reviewed'
+    end
+  end
 end
