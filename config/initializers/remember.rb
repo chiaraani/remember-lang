920cd71850e21aec1b2beb85ae7f4b673ac83556
Chiara Piccinini Núñez <chiaraani@gmail.com>
2020-01-21 23:16:40 +0000

    change version

---
+++ config/initializers/remember.rb
@@ -0,0 +1,6 @@
+module Remember
+  # Minimum increase in time to wait to review
+  INCREASE = 3
+  # Maximum reviews to perform per day
+  MAX_PER_DAY = 20
+end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 module Remember
   module Remember::Lang
-    VERSION = "0.2.6"
+    VERSION = "0.3.0"
   end
 end
