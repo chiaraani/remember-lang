d1ba58ee659ba4f353ec79e9cc8a6f54f2a86277
Chiara Piccinini Núñez <chiaraani@gmail.com>
2020-07-30 15:49:58 +0100

    Word helper shows if it is postponed

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,11 +1,13 @@
 module WordsHelper
-  def next_review(word)
-    if word.reviews.pending.count == 1
-      'It should be reviewed today'
-    elsif word.reviews.notperformed.count == 1
-      'It may be reviewed in ' + pluralize((word.reviews.last.scheduled_for - Date.today).to_i, 'day')
-    else
-      'It may NOT be reviewed'
-    end
-  end
+	def next_review(word)
+		if word.postpone
+			'It is postponed.'
+		elsif word.reviews.pending.count == 1
+			'It should be reviewed today.'
+		elsif word.reviews.notperformed.count == 1
+			'It may be reviewed in ' + pluralize((word.reviews.last.scheduled_for - Date.today).to_i, 'day') + '.'
+		else
+			'There is not a future schedueld review.'
+		end
+	end
 end
--- spec/helpers/words_helper_spec.rb
+++ spec/helpers/words_helper_spec.rb
@@ -6,20 +6,26 @@ RSpec.describe WordsHelper, type: :helper do
     context 'with future reviews' do
       it 'returns in how many days the next review may be performed' do
         word = create(:review).word
-        expect(next_review(word)).to match 'It may be reviewed in 10 days'
+        expect(next_review(word)).to eq 'It may be reviewed in 10 days.'
       end
     end
 
     context 'with reviews ready to perform' do
       it do
         word = create(:pending_review).word
-        expect(next_review(word)).to match 'It should be reviewed today'
+        expect(next_review(word)).to eq 'It should be reviewed today.'
       end
     end
 
     context 'when no reviews to do either in future or today' do
       it do
-        expect(next_review(create(:word))).to match 'It may NOT be reviewed'
+        expect(next_review(create(:word))).to eq 'There is not a future schedueld review.'
+      end
+    end
+
+    context 'with reviews but postponed' do
+      it do
+        expect(next_review(create(:word, postpone: true))).to eq 'It is postponed.'
       end
     end
   end
